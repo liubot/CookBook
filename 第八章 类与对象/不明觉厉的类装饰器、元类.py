2dd311829cf0377cb111b2c94c650b20e55dad48
liubot <liubot@ufsoft.com.cn>
2018-04-12 13:01:09 +0800

    装饰器，元类

--- 第八章 类与对象/不明觉厉的类装饰器、元类.py
+++ 第八章 类与对象/不明觉厉的类装饰器、元类.py
@@ -2,7 +2,7 @@
 # _*_ coding:utf-8 _*_
 
 class Descriptor:
-    def __int__(self,name=None,**opts):
+    def __init__(self,name=None,**opts):
         self.name = name
         for key,value in opts.items():
             setattr(self,key,value)
@@ -14,7 +14,7 @@ class Typed(Descriptor):
     def __set__(self, instance, value):
         if not isinstance(value,self.expected_type):
             raise TypeError("Expected {}".format(self.expected_type))
-        super(Typed, self).__set__(instance,value)
+        super().__set__(instance,value)
 
 class Unsigned(Descriptor):
     def __set__(self, instance, value):
@@ -26,13 +26,12 @@ class MaxSized(Descriptor):
     def __init__(self,name=None,**opts):
         if "size" not in opts:
             raise TypeError("missing size option")
-        # super().__init__(name,**opts)
-        super().__init__()
+        super().__init__(name,**opts)
 
     def __set__(self, instance, value):
         if len(value) >= self.size:
             raise ValueError("size must be < {}".format(self.size))
-        super().__set__(instance.value)
+        super().__set__(instance,value)
 
 
 class Integer(Typed):
@@ -63,7 +62,28 @@ class Stock:
         self.name = name
         self.shares = shares
         self.price = price
-s = Stock("apple",5,12)
+s = Stock("apple",5,12.01)
 print (s.name)
 print (s.shares)
-print (s.price)
+s.shares = 10
+
+'''类装饰器'''
+def check_attributes(**kwargs):
+    def decorate(cls):
+        for key,value in kwargs.items():
+            if isinstance(value,Descriptor):
+                value.name = key
+                setattr(cls,key,value)
+            else:
+                setattr(cls,key,value(key))
+        return cls
+    return decorate
+
+@check_attributes(name=SizeString(size=8),shares=UnsignedInteger,price=UnsignedFloat)
+class Stock2:
+    def __init__(self,name,shares,price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+'''元类'''
